e66ad32ce8615f8d756a90210a8f5887eb572424
Alexey Nabrovenkov <nabrovenkov@gmail.com>
2024-02-08 11:20:06 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import { NewComponent } from './NewComponent';
+
+const student = [
+  {id: 1, name: 'Alex', age: 36},
+  {id: 2, name: 'Borya', age: 18},
+  {id: 3, name: 'Hydra', age: 188},
+]
 
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+      <Header />
+      >Tech
     </div>
   );
 }
